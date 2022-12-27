b27b10c0440d728e03addbc2ef611a5b66217f0c
Juan Jose <juanobando.developer@gmail.com>
2022-12-27 15:36:57 -0500

    importing classes and creating instances of them

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,12 @@
+const account = require("./account");
+const car = require("./car");
+const payment = require("./payment");
+
 console.log("Hello World!");
+
+let acc = new account("Juan", "123456789", "example@example.com", "123456789");
+let auto = new car("ABC123", acc, 4);
+let pay = new payment();
+
+console.log(acc.printDataAccount());
+console.log(auto.printDataCar());
