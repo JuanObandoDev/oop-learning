62030f68933c8890d09e1f3a880480597892c97b
Juan Jose <juanobando.developer@gmail.com>
2022-12-27 14:58:26 -0500

    adding the constructor and toString method

--- JAVA/account.java
+++ JAVA/account.java
@@ -4,4 +4,16 @@ public class account {
     public String document;
     public String email;
     public String password;
+
+    public account(String name, String document, String email, String password) {
+        this.name = name;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return ("Name: " + name + ", Document: " + document + ", Email: " + email + ", Password: " + password);
+    }
 }
