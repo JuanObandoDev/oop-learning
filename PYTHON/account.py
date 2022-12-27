fad748c08b87ac2598ade006b9b927121196817e
Juan Jose <juanobando.developer@gmail.com>
2022-12-27 15:08:03 -0500

    adding init dunder method and toString method

--- PYTHON/account.py
+++ PYTHON/account.py
@@ -1,6 +1,15 @@
-class Accout:
+class Account:
     id = int
     name = str
     document = str
     email = str
     password = str
+
+    def __init__(self, name, document, email, password):
+        self.name = name
+        self.document = document
+        self.email = email
+        self.password = password
+
+    def toString(self):
+        return "Name: " + self.name + ", Document: " + self.document + ", Email: " + self.email + ", Password: " + self.password
--- PYTHON/car.py
+++ PYTHON/car.py
@@ -3,3 +3,11 @@ class Car:
     license = str
     driver = str
     passenger = int
+
+    def __init__(self, license, driver, passenger):
+        self.license = license
+        self.driver = driver
+        self.passenger = passenger
+
+    def toString(self):
+        return "License: " + self.license + ", Driver: " + self.driver + ", Passenger: " + str(self.passenger)
