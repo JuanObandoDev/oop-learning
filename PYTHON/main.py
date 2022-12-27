85fff210f7483dc3ecbdffc2b1fcb870518868d6
Juan Jose <juanobando.developer@gmail.com>
2022-12-27 15:40:51 -0500

    send account type in car constructor

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     print("Hello World")
 
     acc = Account("Juan", "123456789", "example@example.com", "123456")
-    car = Car("AMS234", acc.name, 4)
+    car = Car("AMS234", acc, 4)
     pay = Payment()
     route = Route()
 
