506530b2945550fea9d0503b29745cf2257ddc15
Juan Jose <juanobando.developer@gmail.com>
2022-12-27 15:46:38 -0500

    adding constructor function and print data function

--- PHP/account.php
+++ PHP/account.php
@@ -5,5 +5,16 @@ class account{
     public $document;
     public $email;
     public $password;
+
+    public function __construct($name, $document, $email, $password){
+        $this->name = $name;
+        $this->document = $document;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function printDataAccount(){
+        return "Name: $this->name, Document: $this->document, Email: $this->email, Password: $this->password";
+    }
 }
 ?>
--- PHP/car.php
+++ PHP/car.php
@@ -4,5 +4,15 @@ class car {
     public $license;
     public $driver;
     public $passenger;
+
+    public function __constructor($license, $driver, $passenger) {
+        $this->license = $license;
+        $this->driver = $driver;
+        $this->passenger = $passenger;
+    }
+
+    public function printDataCar() {
+        return "License: $this->license, Driver: {$this->driver->name}, Passenger: $this->passenger";
+    }
 }
 ?>
