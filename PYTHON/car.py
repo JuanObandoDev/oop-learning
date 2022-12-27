930f81951114225a6d471911adb3153de230e79b
Juan Jose <juanobando.developer@gmail.com>
2022-12-27 15:40:15 -0500

    change driver type

--- PYTHON/car.py
+++ PYTHON/car.py
@@ -1,7 +1,9 @@
+from account import Account
+
 class Car:
     id = int
     license = str
-    driver = str
+    driver = Account("","")
     passenger = int
 
     def __init__(self, license, driver, passenger):
@@ -10,4 +12,4 @@ class Car:
         self.passenger = passenger
 
     def toString(self):
-        return "License: " + self.license + ", Driver: " + self.driver + ", Passenger: " + str(self.passenger)
+        return "License: " + self.license + ", Driver: " + self.driver.name + ", Passenger: " + str(self.passenger)
