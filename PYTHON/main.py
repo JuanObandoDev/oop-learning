4fe58743d4a9a1ec054cb1f1ffe15f59d926de83
Juan Jose <juanobando.developer@gmail.com>
2022-12-27 15:08:45 -0500

    creating instances of classes and print their information

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,2 +1,15 @@
+from account import Account
+from car import Car
+from payment import Payment
+from route import Route
+
 if __name__ == "__main__":
     print("Hello World")
+
+    acc = Account("Juan", "123456789", "example@example.com", "123456")
+    car = Car("AMS234", acc.name, 4)
+    pay = Payment()
+    route = Route()
+
+    print(acc.toString())
+    print(car.toString())
