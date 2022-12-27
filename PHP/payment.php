766ae4b386281bc7c8e51a9adb9d5cd470ff3754
Juan Jose <juanobando.developer@gmail.com>
2022-12-27 15:47:05 -0500

    adding constructor function and print data function

--- PHP/payment.php
+++ PHP/payment.php
@@ -1,5 +1,9 @@
 <?php 
 class payment {
     public $id;
+
+    public function printDataPayment() {
+        return "Id: $this->id";
+    }
 }
 ?>
--- PHP/route.php
+++ PHP/route.php
@@ -3,5 +3,14 @@ class route {
     public $id;
     public $start;
     public $end;
+
+    public function __constructor($start, $end) {
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    public function printDataRoute() {
+        return "Start: $this->start, End: $this->end";
+    }
 }
 ?>
