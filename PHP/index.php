1eb2b3ddd4562c97de93a40dbb0e295b469bd76c
Juan Jose <juanobando.developer@gmail.com>
2022-12-27 15:47:50 -0500

    create an instance of classes

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,15 @@
 <?php
     echo "Hello World";
+
+    // Include
+    include('account.php');
+    include('car.php');
+    include('payment.php');
+    include('route.php');
+
+    // Create new account
+    $account = new account("Andres Herrera", "1020304050", "example@example.com", "123456");
+    $car = new car("ABC123", $account, 4);
+    echo $account->printDataAccount();
+    echo $car->printDataCar();
 ?>
