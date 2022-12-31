5114d3f178d389a0d1ced2eb45039d533042eda2
Juan Jose <juanobando.developer@gmail.com>
2022-12-31 17:11:28 -0500

    creating driver and uber cars objects

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -2,6 +2,11 @@ from account import Account
 from car import Car
 from payment import Payment
 from route import Route
+from uberX import UberX
+from uberPool import UberPool
+from uberBlack import UberBlack
+from uberVan import UberVan
+from driver import Driver
 
 if __name__ == "__main__":
     print("Hello World")
@@ -13,3 +18,9 @@ if __name__ == "__main__":
 
     print(acc.toString())
     print(car.toString())
+
+    driver = Driver("Angela", "123456789", "example@example.com", "123456", "#a1b2c3")
+    print(driver.toString())
+
+    uberX = UberX("AMS234", driver, 4, "Chevrolet", "Spark")
+    print(uberX.toString())
