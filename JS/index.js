cb7b6fbce24730a4609c4c3c76bf2756f1c59360
Juan Jose <juanobando.developer@gmail.com>
2022-12-31 17:10:27 -0500

    creating driver and uber cars objects

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,11 @@
 const account = require("./account");
 const car = require("./car");
 const payment = require("./payment");
+const driver = require("./driver");
+const UberX = require("./uberX");
+const UberPool = require("./uberPool");
+const UberVan = require("./uberVan");
+const UberBlack = require("./uberBlack");
 
 console.log("Hello World!");
 
@@ -10,3 +15,16 @@ let pay = new payment();
 
 console.log(acc.printDataAccount());
 console.log(auto.printDataCar());
+
+let dri = new driver(
+  "Angela",
+  "987654321",
+  "example@example.com",
+  "123456789",
+  "#ABC123"
+);
+
+console.log(dri.printDataDriver());
+
+let uberX = new UberX("ABC123", dri, 4, "Chevrolet", "Spark");
+console.log(uberX.printDataCar());
