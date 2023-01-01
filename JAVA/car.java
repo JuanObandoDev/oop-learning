564a79056f2c020ef8d2707009019263107b444d
Juan Jose <juanobando.developer@gmail.com>
2023-01-01 02:51:53 -0500

    adding getters and setters and changing access modifiers

--- JAVA/car.java
+++ JAVA/car.java
@@ -1,13 +1,47 @@
 public class car {
-    public int id;
-    public String license;
-    public driver driver;
-    public int passenger;
+    private int id;
+    private String license;
+    private driver driver;
+    protected int passenger;
 
     public car(String license, driver driver, int passenger) {
+        this.id = 101010;
         this.license = license;
         this.driver = driver;
         this.passenger = passenger;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(driver driver) {
+        this.driver = driver;
+    }
+
+    public int getPassenger() {
+        return this.passenger;
+    }
+
+    public void setPassenger(int passenger) {
+        if (passenger != 4) {
+            System.out.println("You need to assign 4 passengers");
+            return;
+        } else {
+            this.passenger = passenger;
+        }
     }
 
     @Override
--- PHP/car.php
+++ PHP/car.php
@@ -1,15 +1,49 @@
 <?php
 require_once('account.php');
 class car {
-    public $id;
-    public $license;
-    public $driver;
-    public $passenger;
+    private $id;
+    private $license;
+    private $driver;
+    protected $passenger;
 
     public function __constructor($license, $driver, $passenger) {
+        $this->id = 101010;
         $this->license = $license;
         $this->driver = $driver;
         $this->passenger = $passenger;
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function getLicense() {
+        return $this->license;
+    }
+    
+    public function setLicense($license) {
+        $this->license = $license;
+    }
+
+    public function getDriver() {
+        return $this->driver;
+    }
+
+    public function setDriver($driver) {
+        $this->driver = $driver;
+    }
+
+    public function getPassenger() {
+        return $this->passenger;
+    }
+
+    public function setPassenger($passenger) {
+        if ($passenger != 4) {
+            echo "yu need to assing 4 passengers";
+            return;
+        } else {
+            $this->passenger = $passenger;
+        }
     }
 
     public function printDataCar() {
